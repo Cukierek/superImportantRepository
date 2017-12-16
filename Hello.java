b0a7ddcdf661253168dc24c42ed90efd9a02a2a4
Cukierek <cukierek@programmer.net>
2017-12-16 13:04:02 +0100

    poprawione literały

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello ZZZZZ!");
-		System.out.println("Hello YYYY!");
+		System.out.println("Hello WWWW!");
 	}
 	
 	public void bar() {
